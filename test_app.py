36104febb8433cee655301e1e40ffef25be17f75
sandra_kuczynska <san.kuczynska@gmail.com>
2023-12-04 17:40:06 +0000

    Try to fix tests

--- test_app.py
+++ test_app.py
@@ -2,19 +2,18 @@ import unittest
 from unittest.mock import patch
 from app import app, log
 
-
 class Test(unittest.TestCase):
 
     def test_get_page(self):
         response = app.test_client().get('/')
         self.assertIn("<pre>Animals:", response.text)
         assert response.status_code == 200
-
+    
     @patch('app.add_animal')
     def test_add_animal(self, mock_add_animal):
         mock_add_animal.post.side_effect = log.append(f"Mocking add_animal function")
         payload = {
-            "name": "luka",
+            "name":  "luka",
             "animal": "dog",
             "add_animal": "Add Animals"}
         response = app.test_client().post('/', data=payload)
@@ -29,7 +28,6 @@ class Test(unittest.TestCase):
         response = app.test_client().post('/', data=payload)
         self.assertIn("Mocking get_animals function", response.text)
         assert response.status_code == 200
-
 
 if __name__ == '__main__':
     unittest.main()
